da1da6d3d2555441c989e2b27d710180ad23437d
Florian Klein <florian.klein02@gmx.net>
2024-08-02 22:12:06 +0200

    fix: Fixed session state val
    Created a function that safes the search results in a pd.DataFrame instead a list object and adjusted dependencies

--- webapp/webapp.py
+++ webapp/webapp.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import arxiv
 import datetime as dt
+import pandas as pd 
+
 
 st.set_page_config(
     page_title= "Arvix Paper Search",
@@ -19,24 +21,59 @@ if "selected_paper" not in st.session_state:
 if "search_results" not in st.session_state:
     st.session_state.search_results = []
 
+
+# Functions
+#
+#
+def create_search_results_df(search_results) -> pd.DataFrame:
+    """
+    Creates df from list of search results.
+
+    :params:    - search_results (list): list of search results with fixed attributes (title, authors, published, summary, primary_category, categories, links)
+
+    :returns:    - pd.Dataframe: df, each row represents one search result
+
+    :example:   
+    >>> search_results = [Suchergebnis1, Suchergebnis2, ...]
+    >>> df = create_search_results_df(search_results)
+    >>> print(df.head())
+    """
+
+    data = []
+    for r in search_results:
+        data.append({
+            'title': r.title,
+            'authors': ', '.join([author.name for author in r.authors]),
+            'published': r.published,
+            'summary': r.summary,
+            'primary_category': r.primary_category,
+            'categories': ', '.join(r.categories),
+            "links": r.links
+        })
+    return pd.DataFrame(data)
+
 def select_paper(title: str, auth: str, sum: str, pub: dt.date, links: list):
     """
     On click of a paper button, the session state "selected paper" gets updated with the val of the selected paper
 
-    :params:    - title (title of selected paper)
-                - auth (authors ")
-                - sum (abstract ")
-                - pub (publish date)
-                - links (commonly a list of 2 links: 1. abstract/download page of paper, 2. pdf)
+    :params:    - title (str): title of selected paper
+                - auth (str): authors "
+                - sum (str): abstract "
+                - pub (str): publish date
+                - links (list): commonly a list of 2 links (1. abstract/download page of paper, 2. pdf)
+
+    :returns:   - dict: update the attributes of the dict in st.session_state with those of the selected paper
     """
     st.session_state.selected_paper = {"title": title, "authors": auth, "summary": sum, "published": pub, "links": links}
+
+
+# Page content
 
 # Page title
 st.title("Arxiv Paper Search Engine")
 
 # Add welcome message for user
 st.write("Welcome!")
-
 
 # Search bar
 # add doc
@@ -54,9 +91,9 @@ with st.container():
     with col_searchbutton:
         if st.button(label="Search!", use_container_width= False, key="searchbutton", type= "primary"):
             client = arxiv.Client()
-            search = arxiv.Search(query= input_searchbar, max_results= 50, sort_by= arxiv.SortCriterion.SubmittedDate)          # To do: implement feat to limit num of res and sort by 
-            st.session_state.search_results = list(client.results(search))
-
+            search = arxiv.Search(query= input_searchbar, max_results= 50, sort_by= arxiv.SortCriterion.SubmittedDate)               # To do: implement feat to limit num of res and sort by 
+            search_results = list(client.results(search))
+            st.session_state.search_results_df = create_search_results_df(search_results)
 
 # Display results 
 #
@@ -65,19 +102,19 @@ with st.container():
 results_tab, vec_space_tab = st.tabs(["Search results", "Vector space model"])
 
 with results_tab:
-
     col_title, col_info = st.columns(2)
 
-    if st.session_state.search_results:
-        max_title_length = max(len(r.title) for r in st.session_state.search_results)
+    if 'search_results_df' in st.session_state and not st.session_state.search_results_df.empty:
+        max_title_length = max(st.session_state.search_results_df['title'].apply(len))
         max_button_label = 106
 
-    with col_title:
-        con_title = st.container(height= 600, border= False) 
-        with con_title:
-            for idx, r in enumerate(st.session_state.search_results):
-                display_title = (f"{idx+1}." + r.title[:max_button_label] + "...") if len(r.title) > max_button_label else f"{idx+1}." + r.title
-                st.button(label= display_title, key=r.title, on_click= select_paper, args= (r.title, ', '.join([author.name for author in r.authors]), r.summary, str(r.published), r.links), use_container_width= False, help= "Click to see description.")
+        with col_title:
+            con_title = st.container(height=600, border=False)
+            with con_title:
+                for idx, row in st.session_state.search_results_df.iterrows():
+                    display_title = (f"{idx+1}." + row['title'][:max_button_label] + "...") if len(row['title']) > max_button_label else f"{idx+1}." + row["title"]
+                    st.button(label= display_title, key= display_title, on_click= select_paper, args= (row["title"], row["authors"], row["summary"], str(row["published"]), row["links"]), use_container_width= False, help= "Click to see description.")
+
 
     with col_info:
         con_info = st.container(border= False)
@@ -88,6 +125,7 @@ with results_tab:
                 st.write(st.session_state.selected_paper["summary"])
                 st.write(f"Link to abs: {st.session_state.selected_paper["links"][0]}")
                 st.write(f"Link to pdf: {st.session_state.selected_paper["links"][1]}")
+
 
 with vec_space_tab:
 
