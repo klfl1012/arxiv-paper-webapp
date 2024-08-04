3df93fd063c20a325ae9fc33c9e31953cb2cb04d
Florian Klein <florian.klein02@gmx.net>
2024-08-04 17:15:37 +0200

    feat: Implemented more query options for user and updated vec space vis
    Changes made:
    - added a slider element to get user input for max search results
    - added a radio buttons to get user input to sort query by Relevance, Last Updated and Submitted Date
    - added radio buttons to get user input to sort resulting query by ascending or descending
    - changed plot libary of scatter_3d from plotly.express to plotly.graph_objects, plot is now constructed by seperate traces each representing a unique category, each cat gets their unique color code generated by a function beforehand.

--- webapp/webapp.py
+++ webapp/webapp.py
@@ -2,7 +2,10 @@ import streamlit as st
 import arxiv
 import datetime as dt
 import pandas as pd 
+
 import plotly.express as px
+import plotly.graph_objects as go
+import colorsys
 
 from transformers import BertTokenizer, BertModel
 import torch
@@ -43,9 +46,9 @@ def create_search_results_df(search_results) -> pd.DataFrame:
     :returns:    - pd.Dataframe: df, each row represents one search result
 
     :example:   
-    >>> search_results = [Suchergebnis1, Suchergebnis2, ...]
-    >>> df = create_search_results_df(search_results)
-    >>> print(df.head())
+        >>> search_results = [Suchergebnis1, Suchergebnis2, ...]
+        >>> df = create_search_results_df(search_results)
+        >>> print(df.head())
     """
 
     data = []
@@ -75,16 +78,35 @@ def select_paper(title: str, auth: str, sum: str, pub: dt.date, links: list):
     """
     st.session_state.selected_paper = {"title": title, "authors": auth, "summary": sum, "published": pub, "links": links}
 
+def generate_unique_colors(num_colors: int) -> list:
+    """
+    Generates a list of unique colors in hex format.
+
+    :params:    - num_colors (int): number of colors to generate
+    :returns:   - list: list of unique colors in hex format
+    :example:
+        >>> colors = generate_unique_colors(5)
+        >>> print(colors)
+    """        
+    colors = []
+    for i in range(num_colors):
+        hue = i / num_colors
+        lightness = 0.5
+        saturation = 0.9
+        rgb = colorsys.hls_to_rgb(hue, lightness, saturation)
+        hex_color = '#{:02x}{:02x}{:02x}'.format(int(rgb[0]*255), int(rgb[1]*255), int(rgb[2]*255))
+        colors.append(hex_color)
+    return colors
+
 def select_row():
     pass
     
-
 
 # Page title
 st.title("Arxiv Paper Search Engine")
 
 # Add welcome message for user
-st.write("Welcome!")
+st.write("Welcome!") # add this to sidebar 
 
 # Search bar
 # add doc
@@ -94,7 +116,15 @@ col_searchbar, col_searchbutton = st.columns([3, 1])
 
 with st.container():
     with col_searchbar:
-        input_searchbar = st.text_input(label= "searchbar", placeholder= "Your query", key="searchbar_input", label_visibility= "collapsed")
+        input_searchbar = st.text_input(label= "searchbar", placeholder= "Your query", key="searchbar_input", label_visibility= "collapsed", help= "Enter your search query here.")
+
+        col_max_results, col_sort_by, col_sort_order = st.columns(3)
+        with col_max_results:
+            input_max_results = st.slider(label= "max_results", min_value= 50, max_value= 2000, value= 100, step= 50, key= "max_results_slider", label_visibility= "collapsed", help= "Choose the number of search results to display.")
+        with col_sort_by:
+            input_sort_by = st.radio(label= "sort_by", options= ["Relevance", "Last Updated Date", "Submitted Date"], key= "sort_by_selectbox", label_visibility= "collapsed", horizontal= True, help= "Choose the sorting criterion.")
+        with col_sort_order:
+            input_sort_order = st.radio(label= "sort_order", options= ["Descending", "Ascending"], key= "sort_order_selectbox", label_visibility= "collapsed", horizontal= True, help= "Choose the sorting order.")
 
         with st.expander("INFO: Query construction"):
             st.write("For a detailed query constructions guide see the official arxiv api documentation 5.1 Details of Query Constructions https://info.arxiv.org/help/api/user-manual.html#query_details")
@@ -102,7 +132,12 @@ with st.container():
     with col_searchbutton:
         if st.button(label="Search!", use_container_width= False, key="searchbutton", type= "primary"):
             client = arxiv.Client()
-            search = arxiv.Search(query= input_searchbar, max_results= 50, sort_by= arxiv.SortCriterion.SubmittedDate)               # To do: implement feat to limit num of res and sort by 
+            search = arxiv.Search(
+                query= input_searchbar, 
+                max_results= input_max_results, 
+                sort_by= arxiv.SortCriterion.Relevance if input_sort_by == "Relevance" else arxiv.SortCriterion.UpdatedDate if input_sort_by == "Last Updated Date" else arxiv.SortCriterion.SubmittedDate, 
+                sort_order= arxiv.SortOrder.Descending if input_sort_order == "Descending" else arxiv.SortOrder.Ascending
+                )                
             search_results = list(client.results(search))
             st.session_state.search_results_df = create_search_results_df(search_results)
 
@@ -162,31 +197,42 @@ with vec_space_tab:
 
         pca = PCA(n_components= 3)
         embeddings_reduced = pca.fit_transform(st.session_state.search_results_df["emb_ti_auth_sum"].tolist())
+        st.session_state.search_results_df["emb_ti_auth_sum_reduced"] = embeddings_reduced.tolist()
 
-        fig = px.scatter_3d(
-                data_frame= st.session_state.search_results_df,
-                x= embeddings_reduced[:, 0], 
-                y= embeddings_reduced[:, 1], 
-                z= embeddings_reduced[:, 2],
-                title= "Search results in 3D",
-                color= st.session_state.search_results_df["primary_category"],
-                hover_data= ["title", "authors", "published"],
-                custom_data= ["title", "authors", "published"]
-        ).update_layout(
-            width= 2000,
-            height= 800,
-            scene= dict(camera= dict(eye= dict(x= 1.2, y= 1.2, z= 1.2)))
-        )
+        unique_categories = st.session_state.search_results_df["primary_category"].unique()
+        colors = generate_unique_colors(len(unique_categories))
+        color_mapping = {category: color for category, color in zip(unique_categories, colors)}
 
+        traces = []
+        for category in unique_categories:
+            category_mask = st.session_state.search_results_df["primary_category"] == category
+            trace = go.Scatter3d(
+                x=embeddings_reduced[category_mask, 0],
+                y=embeddings_reduced[category_mask, 1],
+                z=embeddings_reduced[category_mask, 2],
+                mode="markers+text",
+                marker=dict(
+                    size=5,
+                    color= color_mapping.get(category, "black"),
+                ),
+                text= st.session_state.search_results_df[category_mask].index.values,
+                name= category
+            )
+            traces.append(trace)
+
+        fig = go.Figure(data= traces, layout= dict(width= 2000, height= 600, title= "3D PCA of paper embeddings", legend_title= dict(text= "Categories"), scene= dict(camera= dict(eye= dict(x= .7, y= .7, z= .7)))))
 
         st.plotly_chart(fig, selection_mode= "points")
-
         st.write(st.session_state.search_results_df)
 
 # To do's:
 #
-# 1. Implement feat to limit num of res and sort by
-# 2. Implement feat for cos sim between selected data points in vec space tab
-# 3. Implement tab for user to compare generated abs with original abs 
-# 4. Fix warning occuring when embedding paper info with bert
+# - Implement feat to show embeddings by category (now) or by none (all in one color an click on point to highlight paper in df) 
+# - Implement feat to cluster paper in emebdding space (on-off-switch to activate color by cluster)
+# - Implement feat for cos sim between selected data points in vec space tab (only available if 2 points are selected -> all in one color mode -> calc for every point in df an give option to sort -> see two papers that are most sim)
+
+# - Fix warning occuring when embedding paper info with bert
+
+# - Fine tune own model with arxiv data (lama model), model should be able to generate abstracts from papers (user selects paper via search, paper gets downlaoded and fed into model, model generates abstract, user can compare generated abstract with original abstract and vote which is better)
+# - Implement tab for user to compare generated abs with original abs 
 
